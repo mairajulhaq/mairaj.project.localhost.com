1f4167b9820f03e282bdba861dc246940494c74b
mairajulhaq <mairajulhaq.fed@gmail.com>
2025-04-17 07:19:09 +0500

    Mairaj 17-April-2025 - Commit - 02

---
+++ resources/app-ui/src/pages/admin-app/categories-management/create-category.js
@@ -0,0 +1,417 @@
+import {
+    PageContainer,
+    FooterToolbar,
+    ProCard,
+    ProForm,
+    ProFormText,
+    ProFormTextArea,
+    ProFormList,
+    ProFormSelect
+} from '@ant-design/pro-components';
+import {Row, Col, message, Button, Form, Image, Upload} from 'antd';
+import {UploadOutlined} from '@ant-design/icons';
+import React, {useEffect, useRef, useState} from "react";
+import {request, history} from '@umijs/max';
+
+import { getFile, getBase64 } from '@/components/Helpers/ImageConversion';
+
+
+const waitTime = (time = 100) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true);
+        }, time);
+    });
+};
+
+
+/**
+ * Form Submission handler and API Request Performer
+ */
+const onFinishHandlerForm = async (values, imageUrl) => {
+    console.log('onFinishHandlerForm');
+    console.log('Received values of form: ', values);
+
+    /**
+     * API Request
+     */
+    try {
+
+        const request_data = {
+            image_url: imageUrl,
+            title: values?.title,
+            description: values?.description,
+            author_id: 1,
+            tutors: values?.tutors?.map( ( tutor ) => tutor?.tutor ) || [],
+            users: values?.users?.map( ( user ) => user?.user ) || [],
+        };
+
+        console.log('request_data');
+        console.log(request_data);
+
+        return await request('/api/categories', {
+            method: 'POST',
+            data: request_data,
+
+        }).then(async (api_response) => {
+            console.log('api_response');
+            console.log(api_response);
+
+            /**
+             * User Created then show message and redirect to listing screen
+             */
+            if (api_response?.data?.id > 0) {
+                message.success('Submitted successfully');
+                history.push('/admin-app/categories/edit/' + api_response?.data?.id);
+            }
+
+        }).catch(function (error) {
+            console.log(error);
+        });
+
+    } catch (api_response) {
+        console.log('api_response - error');
+        console.log(api_response);
+    }
+
+    return true;
+    
+};
+
+
+const CreateCategory = () => {
+
+    /**
+     * States of Component
+     */
+
+    const [form] = Form.useForm();
+
+    const [imageUrl, setImageUrl] = useState(DEFAULT_PLACEHOLDER_IMAGE_URL);
+
+    const [ allTutors, setAllTutors ] = useState( [] );
+    const [ allUsers, setAllUsers ] = useState( [] );
+
+    const initialValues = {
+        tutors: [
+			{
+				tutor: undefined, // Shows 1 empty dropdown
+			},
+		],
+		users: [
+			{
+				user: undefined, // Shows 1 empty dropdown
+			},
+		]
+	};
+
+
+    /**
+	 * Start - Tutors Data
+	 */
+	useEffect( () => {
+		return request('/api/users', {
+
+            params: {
+                role: 'tutor',
+                page: 1,
+                per_page: 1000,
+            },
+
+        }).then( ( api_response ) => {
+			const table_data = api_response.data.data.map( ( item, i ) => ( {
+				value: item?.id,
+				label: item?.name,
+			} ) );
+
+			setAllTutors( table_data );
+
+			return table_data;
+		} );
+	}, [] );
+
+    
+    /**
+	 * Start - Users Data
+	 */
+	useEffect( () => {
+		return request('/api/users', {
+
+            params: {
+                role: 'user',
+                page: 1,
+                per_page: 1000,
+            },
+
+        }).then( ( api_response ) => {
+			const table_data = api_response.data.data.map( ( item, i ) => ( {
+				value: item?.id,
+				label: item?.name,
+			} ) );
+
+			setAllUsers( table_data );
+
+			return table_data;
+		} );
+	}, [] );
+
+
+    /**
+     * The Component Output
+     */
+
+    return (
+        <PageContainer>
+            
+                <ProForm
+                    onFinish={async (values) => {
+                        await waitTime(2000);
+
+                        await onFinishHandlerForm(values, imageUrl);
+                    }}
+                    layout='vertical'
+                    grid={true}
+                    initialValues={initialValues}
+                    form={form}
+                    submitter={{
+                        render: (_, dom) => <FooterToolbar>{dom}</FooterToolbar>,
+                    }}
+                >
+                
+                    <ProCard
+                        title="Category Details"
+                        bordered
+                        headerBordered
+                        collapsible
+                        size="default"
+                        type="inner"
+                        style={{
+                            marginBlockEnd: 15,
+                            minWidth: 800,
+                            maxWidth: '100%',
+                        }}
+                        onCollapse={(collapse) => console.log(collapse)}
+                    >
+                        <Row
+                            gutter={{
+                                xs: 8,
+                                sm: 16,
+                                md: 24,
+                                lg: 32,
+                            }}
+                        >
+                            <Col span={6}>
+                                <ProForm.Group size={24}>
+                                    <Col span={24}>
+                                        <Image
+                                            width='100%'
+                                            height={200}
+                                            src={imageUrl}
+                                        />
+                                    </Col>
+                                    <Col span={24} style={{ textAlign: 'center'}}>
+                                    {/* getValueFromEvent={getFile} */}
+                                        <ProForm.Item name='image_url' getValueFromEvent={getFile} > 
+                                            <Upload
+                                                name={'file'}
+                                                onChange={(info) => {
+
+                                                    console.log('file - onChange - info');
+                                                    console.log(info);
+
+                                                    if (info.file.status === 'uploading') {
+                                                        return;
+                                                    }
+                                                    if( info.file.status == "removed" ){
+                                                        setImageUrl('');
+                                                    }
+                                            
+                                                    if (info.file.status === 'done') {
+
+
+                                                        setImageUrl(info?.file?.response?.url); 
+
+                                                        // getBase64(info).then((base64String) => {
+                                                        //     console.log('base64String');
+                                                        //     console.log(base64String);
+                                                        //     setImageUrl(base64String);
+                                                        // });
+                                            
+                                                    }
+                                            
+                                                    if (info.file.status === 'error') {
+                                                        message.error(`${info.file.name} file upload failed.`);
+                                                    }
+                                            
+                                                }}
+                                                maxCount={1}
+                                                action="/api/file-manager/upload"
+                                                // customRequest={(event) => {
+                                                //     console.log('event = file is uploading');
+                                                //     console.log(event);
+
+                                                //     console.log('event?.file');
+                                                //     console.log(event?.file);
+
+                                                //     request('/api/file-manager/upload', {
+                                                //         method: 'POST',
+                                                //         data: {
+                                                //            'filename': event?.file?.name,
+                                                //             // 'directory': 'users' 
+                                                //             ...event?.data
+                                                //         },
+                                                //     }).then(async (api_response) => {
+                                                //         console.log('api_response');
+                                                //         console.log(api_response);
+                                                //     });
+
+                                                // }}
+                                                data={{
+                                                    'directory': 'categories'
+                                                }}
+                                                onRemove={(file) => {
+                                                    console.log('file is removed');
+                                                    console.log(file);
+
+                                                    request('/api/file-manager/delete', {
+                                                        method: 'DELETE',
+                                                        data: {
+                                                           'filename': file?.name,
+                                                            'directory': 'categories' 
+                                                        },
+                                                    }).then(async (api_response) => {
+                                                        console.log('api_response');
+                                                        console.log(api_response);
+                                                    });
+
+                                                }}
+                                            >
+                                                <Button
+                                                    type="primary"
+                                                    icon={<UploadOutlined/>}
+                                                    style={{'margin': '10px 0px 0px 0px'}}
+                                                    onClick={(event) => {}}
+                                                >
+                                                    Change Image
+                                                </Button>
+                                            </Upload>
+                                        </ProForm.Item>
+                                    </Col>
+                                </ProForm.Group>
+                            </Col>
+                            <Col span={18}>
+                                <ProForm.Group size={24}>
+                                    <ProFormText
+                                        name={'title'}
+                                        label="Title"
+                                        placeholder="Type Category Title"
+                                        colProps={{xs: 24, sm: 24, md: 24, lg: 24, xl: 24}}
+                                    />
+                                </ProForm.Group>
+                                <ProForm.Group size={24}>
+                                    <ProFormTextArea
+                                        name={'description'}
+                                        label="Description"
+                                        placeholder="Share a little description to fill out your category."
+                                        colProps={{xs: 24, sm: 24, md: 24, lg: 24, xl: 24}}
+                                    />
+                                </ProForm.Group>
+                            </Col>
+                        </Row>
+                    </ProCard>
+                    <ProCard
+                        title="Assign Tutors"
+                        bordered
+                        headerBordered
+                        collapsible
+                        size="default"
+                        type="inner"
+                        style={ {
+                            marginBlockEnd: 15,
+                            minWidth: 800,
+                            maxWidth: '100%',
+                        } }
+                    >
+                        <ProForm.Group size={ 24 }>
+                            <ProFormList
+                                name={ 'tutors' }
+                                min={ 1 }
+                                copyIconProps={ { tooltipText: 'Copy this tutor' } }
+                                deleteIconProps={ { tooltipText: 'Delete this tutor' } }
+                                creatorButtonProps={ {
+                                    creatorButtonText: 'Add New Tutor',
+                                } }
+                            >
+                                <ProForm.Group size={ 24 }>
+                                    <ProFormSelect
+                                        name={ 'tutor' }
+                                        label="Tutor"
+                                        showSearch
+                                        options={ allTutors }
+                                        debounceTime={ 300 }
+                                        placeholder="Please Select a Tutor"
+                                        rules={ [ { required: true } ] }
+                                        colProps={ {
+                                            xs: 24,
+                                            sm: 24,
+                                            md: 24,
+                                            lg: 24,
+                                            xl: 24,
+                                        } }
+                                    />
+                                </ProForm.Group>
+                            </ProFormList>
+                        </ProForm.Group>
+                    </ProCard>
+                    <ProCard
+                        title="Assign Users"
+                        bordered
+                        headerBordered
+                        collapsible
+                        size="default"
+                        type="inner"
+                        style={ {
+                            marginBlockEnd: 15,
+                            minWidth: 800,
+                            maxWidth: '100%',
+                        } }
+                    >
+                        <ProForm.Group size={ 24 }>
+                            <ProFormList
+                                name={ 'users' }
+                                min={ 1 }
+                                copyIconProps={ { tooltipText: 'Copy this user' } }
+                                deleteIconProps={ { tooltipText: 'Delete this user' } }
+                                creatorButtonProps={ {
+                                    creatorButtonText: 'Add New User',
+                                } }
+                            >
+                                <ProForm.Group size={ 24 }>
+                                    <ProFormSelect
+                                        name={ 'user' }
+                                        label="User"
+                                        showSearch
+                                        options={ allUsers }
+                                        debounceTime={ 300 }
+                                        placeholder="Please Select a User"
+                                        rules={ [ { required: true } ] }
+                                        colProps={ {
+                                            xs: 24,
+                                            sm: 24,
+                                            md: 24,
+                                            lg: 24,
+                                            xl: 24,
+                                        } }
+                                    />
+                                </ProForm.Group>
+                            </ProFormList>
+                        </ProForm.Group>
+                    </ProCard>
+                </ProForm>
+                
+        </PageContainer>
+    );
+
+};
+
+export default CreateCategory;
---
+++ resources/app-ui/src/pages/admin-app/categories-management/list-categories.js
@@ -0,0 +1,197 @@
+import {EditOutlined, PlusOutlined} from '@ant-design/icons';
+import {
+  PageContainer,
+  ProTable
+} from '@ant-design/pro-components';
+import {Button, Image, message} from 'antd';
+import {request,history} from '@umijs/max';
+import moment from 'moment';
+import {useModel} from 'umi';
+import {useRef, useState} from "react";
+
+import DeleteCategory from './delete-category';
+
+
+export const waitTimePromise = async (time = 100) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true);
+        }, time);
+    });
+};
+
+export const waitTime = async (time = 100) => {
+    await waitTimePromise(time);
+};
+
+const ListCategories = () => {
+
+    const {initialState, loading, refresh, setInitialState} = useModel('@@initialState');
+
+    console.log('initialState');
+    console.log(initialState);
+
+    console.log('loading');
+    console.log(loading);
+
+    const categoriesTableRef = useRef();
+
+    const columns = [
+
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'table-column-id',
+            hideInSearch: true,
+            sorter: true,
+            defaultSortOrder: 'descend',
+        },
+        {
+            title: "Title",
+            key: 'table-column-title',
+            copyable: true,
+            hideInSearch: true,
+            render: (dom, record) => {
+
+                return (
+                    <div>
+
+                        <Image
+                            width={50}
+                            height={50}
+                            src={record?.image_url}
+                        />
+                        <span style={{margin: "0px 0px 0px 10px"}}>
+                            {record?.title}
+                        </span>
+
+                    </div>
+                );
+            },
+        },
+        {
+            title: "Created Date",
+            dataIndex: 'created_at',
+            key: 'table-column-created-date',
+            sorter: true,
+            hideInForm: true,
+            hideInSearch: true,
+            render: (created_at) => {
+              return (<p>{moment( new Date(created_at) ).format('DD-MM-YYYY')}</p>)
+            },
+        },
+        {
+            title: "Updated Date",
+            dataIndex: 'updated_at',
+            key: 'table-column-updated-date',
+            sorter: true,
+            hideInForm: true,
+            hideInSearch: true,
+            render: (updated_at) => {
+              return (<p>{moment( new Date(updated_at) ).format('DD-MM-YYYY')}</p>)
+            },
+        },
+        {
+            title: 'Actions',
+            valueType: 'option',
+            key: 'table-column-actions',
+            render: (text, record, _, action) => [
+
+                <Button
+                    key="editable"
+                    onClick={() => {
+                        history.push('/admin-app/categories/edit/' + record.id);
+                    }}
+                >
+                    <EditOutlined />
+                </Button>,
+
+                <DeleteCategory
+                    rowId={ record?.id }
+                    onFinish={ ( { status, text_message } ) => {
+                        if ( status ) {
+                            message.success( text_message );
+                            categoriesTableRef.current?.reload();
+                        } else {
+                            message.error( text_message );
+                        }
+                    } }
+                />,
+
+            ],
+        },
+    ];
+
+    return (
+        <PageContainer>
+            <ProTable
+                actionRef={categoriesTableRef}
+                rowKey="id"
+                search={false}
+                pagination={{
+                    defaultPageSize: 10,
+                    showSizeChanger: true,
+                    pageSizeOptions: [10, 20, 50, 100],
+                    onChange: (page) => console.log(page),
+                }}
+                toolBarRender={() => [
+                    <Button
+                        type="primary"
+                        key="new"
+                        onClick={() => {
+                            history.push('/admin-app/categories/new');
+                        }}
+                    >
+                        <PlusOutlined/> New
+                    </Button>,
+                ]}
+                request={
+
+                        async (params, sort, filter) => {    
+
+                        console.log('params');
+                        console.log(params);
+
+                        console.log('params - sort');
+                        console.log(sort);
+
+                        console.log('params - filter');
+                        console.log(filter);
+
+                        /**
+                         * Delay the API request
+                         */
+                        await waitTime(2000);
+
+                        return await request('/api/categories', {
+
+                            params: {
+                                sort: {...sort},
+                                pagination: {...params},
+                            },
+
+                        }).then(async (api_response) => {
+                            console.log('api_response');
+                            console.log(api_response);
+
+                            console.log('api_response.data');
+                            console.log(api_response.data);
+
+                            console.log('api_response.data.data');
+                            console.log(api_response.data.data);
+
+                            return { data: api_response.data.data, total: api_response.data.total, current_page: api_response.data.current_page};
+
+                        }).catch(function (error) {
+                            console.log(error);
+                        });
+
+                    }}
+                columns={columns}
+            />
+
+        </PageContainer>
+    );
+};
+
+export default ListCategories;
